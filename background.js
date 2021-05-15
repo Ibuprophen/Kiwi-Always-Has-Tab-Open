e6a8b027163ae96fb44cbf5e94e6225b6e62b379
harsh <ycdtharsh@gmail.com>
2021-05-15 16:42:02 +0530

    proper working extension

--- background.js
+++ background.js
@@ -1,13 +1,30 @@
-chrome.tabs.onRemoved.addListener(() => {
-  chrome.tabs.query({currentWindow: true}, (tabs) => {
-    console.log(tabs);
+try{
+  let tabCreated = null;
+  chrome.tabs.onRemoved.addListener(() => {
+    chrome.tabs.query({currentWindow: true}, (tabs) => {
+      console.log(tabs);
 
-    if( tabs.length === 1 ){
-      chrome.tabs.create({
-        url: 'chrome://newtab',
-        active: false,
-      })
-    }
+      if( tabs.length === 1 ){
+        chrome.tabs.create({ url: 'chrome://newtab/', active: false, }, function(tab){
+          tabCreated = tab;
+          console.log("newly created tab is: ", tabCreated);
+        })
+      }
+    })
   })
-})
 
+  chrome.tabs.onCreated.addListener((tab) => {
+    if(tabCreated !== null && tab.id !== tabCreated.id){
+      if(tabCreated.url === 'chrome://newtab/'){
+        chrome.tabs.remove(tabCreated.id, () => console.log("removed tab"));
+      }
+    }
+  });
+
+  chrome.tabs.onUpdated.addListener((tabId, changeInfo, tabInfo) => {
+    if(tabId === tabCreated.id)
+      tabCreated = tabInfo;
+  });
+}catch(error){
+  console.log(error);
+}
