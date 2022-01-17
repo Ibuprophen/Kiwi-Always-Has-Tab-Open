e89a0e62c276f555fcedfa02ea2c279e9ec1f74b
Delphox <smugleaf@live.com>
2022-01-17 12:23:41 -0300

    Update background.js

--- background.js
+++ background.js
@@ -1,30 +1,11 @@
-try{
   let tabCreated = null;
   chrome.tabs.onRemoved.addListener(() => {
     chrome.tabs.query({currentWindow: true}, (tabs) => {
-      console.log(tabs);
 
-      if( tabs.length === 1 ){
-        chrome.tabs.create({ url: 'chrome://newtab/', active: false, }, function(tab){
+      if( tabs.length === 0 ){
+        chrome.tabs.create({ url: 'chrome-search://local-ntp/local-ntp.html', active: false, }, function(tab){
           tabCreated = tab;
-          console.log("newly created tab is: ", tabCreated);
         })
       }
     })
   })
-
-  chrome.tabs.onCreated.addListener((tab) => {
-    if(tabCreated !== null && tab.id !== tabCreated.id){
-      if(tabCreated.url === 'chrome://newtab/'){
-        chrome.tabs.remove(tabCreated.id, () => console.log("removed tab"));
-      }
-    }
-  });
-
-  chrome.tabs.onUpdated.addListener((tabId, changeInfo, tabInfo) => {
-    if(tabId === tabCreated.id)
-      tabCreated = tabInfo;
-  });
-}catch(error){
-  console.log(error);
-}
