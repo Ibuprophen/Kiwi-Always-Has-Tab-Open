7b4aa7e913aeb7095c84b650fc5b0bf3e3a7d60d
Delphox <smugleaf@live.com>
2022-01-17 21:25:32 -0300

    try to prevent multiple tabs from spawning at once when clearing

--- background.js
+++ background.js
@@ -1,11 +1,15 @@
   let tabCreated = null;
+  var count = 0;
+  
   chrome.tabs.onRemoved.addListener(() => {
     chrome.tabs.query({currentWindow: true}, (tabs) => {
 
-      if( tabs.length === 0 ){
+      if( tabs.length === 0 && count === 0){
         chrome.tabs.create({ url: 'chrome-search://local-ntp/local-ntp.html', active: false, }, function(tab){
           tabCreated = tab;
         })
+		count = 1;
+		setTimeout(function(){	count = 0;	}, 500);
       }
     })
   })
