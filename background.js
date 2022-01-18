b0f824371aea62ba94e8937d0ecf246b91c4eab6
Delphox <smugleaf@live.com>
2022-01-17 22:06:58 -0300

    safer to reset the count before start creating tab

--- background.js
+++ background.js
@@ -1,15 +1,15 @@
-  let tabCreated = null;
-  var count = 0;
-  
-  chrome.tabs.onRemoved.addListener(() => {
-    chrome.tabs.query({currentWindow: true}, (tabs) => {
+let tabCreated = null;
+var count = 0;
 
-      if( tabs.length === 0 && count === 0){
-        chrome.tabs.create({ url: 'chrome-search://local-ntp/local-ntp.html', active: false, }, function(tab){
-          tabCreated = tab;
-        })
-            count = 1;
-            setTimeout(function(){ count = 0; }, 500);
-      }
-    })
-  })
+chrome.tabs.onRemoved.addListener(() => {
+chrome.tabs.query({currentWindow: true}, (tabs) => {
+
+  if( tabs.length === 0 && count === 0){
+	count = 1;
+	chrome.tabs.create({ url: 'chrome-search://local-ntp/local-ntp.html', active: false, }, function(tab){
+	  tabCreated = tab;
+	})
+	setTimeout(function(){ count = 0; }, 500);
+  }
+})
+})
